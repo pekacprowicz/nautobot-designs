7d489bc7c0204869c197142c2848abb12416ba12
Przemyslaw Kacprowicz <przemyslaw.kacprowicz@codilime.com>
2025-09-22 08:59:42 +0200

    updates

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,8 +1,8 @@
 """The __init__.py module is required for Nautobot to load the jobs via Git."""
 
 from .core_site import CoreSiteDesign
-from .pool import PoolDesign
-from .racks import RacksDesign
+from .pool import PoolCreateDesign
+from .racks import BuildRackSwitchDesign
 
 __all__ = [
     "CoreSiteDesign",
